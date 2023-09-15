ddffb7219c7a65b9e727555b5f797df7cd3ad99e
Vladyslav <rubakvlad7@gmail.com>
2023-09-15 18:53:43 +0300

    changed axios url to http

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://talkspace.eu-north-1.elasticbeanstalk.com",
+  baseURL: "http://talkspace.eu-north-1.elasticbeanstalk.com",
 });
