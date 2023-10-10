4388f52346d096bfc96d784dad498225454c3ac4
Vladyslav Rybak <rubakvlad7@gmail.com>
2023-10-10 19:03:58 +0300

    changed login url

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import { useRef, useState, useEffect, useContext } from "react";
 import AuthContext from "../../context/AuthProvider";
 
 import axios from "../../api/axios";
-const LOGIN_URL = "/chat-groups/";
+const LOGIN_URL = "/auth/login";
 
 import Header from "../../parts/Header";
 
@@ -28,7 +28,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.get(
+      const response = await axios.post(
         LOGIN_URL,
         JSON.stringify({ user, pwd }),
         {
